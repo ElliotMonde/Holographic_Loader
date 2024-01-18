ca21237f0dbbd3079d874e773c04f0d26e60c030
ElliotMonde <elliotphua@gmail.com>
2024-01-18 12:51:10 +0800

    modularised function, updated lightings to show material and vibrancy, repositioned loaded model

--- js/main.js
+++ js/main.js
@@ -2,9 +2,6 @@ import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 import WebGL from "three/addons/capabilities/WebGL.js";
-// import Stats from 'three/examples/jsm/libs/stats.module'
-
-let model_1, model_2, model_3, model_4;
 
 if (WebGL.isWebGLAvailable()) {
     const scene = new THREE.Scene();
@@ -17,8 +14,13 @@ if (WebGL.isWebGLAvailable()) {
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
-    // const light = new THREE.PointLight(0xffffff, 0.1);
-    scene.background = new THREE.Color(0x006600);
+    scene.background = new THREE.Color(0x000000);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(ambientLight);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 15); // color, intensity
+    directionalLight.position.set(1, 1, 1).normalize();
+    scene.add(directionalLight);
+
 
     function inProgress(xhr) {
         if (xhr.lengthComputable) {
@@ -27,7 +29,6 @@ if (WebGL.isWebGLAvailable()) {
         }
     }
 
-    let butterfly_model;
     const loader = new GLTFLoader();
     const dracoLoader = new DRACOLoader();
     dracoLoader.setDecoderPath("/examples/jsm/libs/draco/");
@@ -35,108 +36,46 @@ if (WebGL.isWebGLAvailable()) {
 
     camera.position.z = 100;
 
+    function loadModel(url, x_pos = null, y_pos = null) {
+        loader.load(
+            url,
+            function (gltf) {
+                const model = gltf.scene;
+                model.scale.set(0.2, 0.2, 0.2);
 
-    // Loading object
-    // const geometry = new THREE.BoxGeometry(10, 10, 10);
-    // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    // const cube_top_left = new THREE.Mesh(geometry, material);
-    // const cube_top_right = new THREE.Mesh(geometry, material);
-    // const cube_bottom_left = new THREE.Mesh(geometry, material);
-    // const cube_bottom_right = new THREE.Mesh(geometry, material);
+                if (x_pos != null) {
+                    model.position.x = x_pos;
+                }
+                if (y_pos != null) {
+                    model.position.y = y_pos;
 
-    loader.load(
-        'assets/butterfly.glb',
-        async function (gltf) {
-            model_1 = gltf.scene;
+                }
+                scene.add(model);
+            },
+            (xhr) => {
+                console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+            },
+            (error) => {
+                console.log(error);
+            }
+        )
+    }
+    let url = 'assets/butterfly.glb';
+    loadModel(url, -30, 8);
+    loadModel(url, -30, -25);
+    loadModel(url, 30, 8)
+    loadModel(url, 30, -25)
 
-            model_1.scale.set(0.1, 0.1, 0.1);
-            model_1.position.x = -50;
-            scene.add(model_1);
-        },
-        (xhr) => {
-            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-        },
-        (error) => {
-            console.log(error);
-        }
-    );
 
-    loader.load(
-        'assets/butterfly.glb',
-        async function (gltf) {
-            model_2 = gltf.scene;
-
-            model_2.scale.set(0.1, 0.1, 0.1);
-            model_2.position.x = -50;
-            model_2.position.y = -30;
-            scene.add(model_2);
-        },
-        (xhr) => {
-            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-        },
-        (error) => {
-            console.log(error);
-        }
-    );
-
-    loader.load(
-        'assets/butterfly.glb',
-        async function (gltf) {
-            model_3 = gltf.scene;
-
-            model_3.scale.set(0.1, 0.1, 0.1);
-            model_3.position.x = 50;
-            scene.add(model_3);
-        },
-        (xhr) => {
-            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-        },
-        (error) => {
-            console.log(error);
-        }
-    );
-
-    loader.load(
-        'assets/butterfly.glb',
-        async function (gltf) {
-            model_4 = gltf.scene;
-
-            model_4.scale.set(0.1, 0.1, 0.1);
-            model_4.position.x = 50;
-            model_4.position.y = -30;
-            scene.add(model_4);
-        },
-        (xhr) => {
-            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-        },
-        (error) => {
-            console.log(error);
-        }
-    );
     camera.position.z = 50;
-
-    // const stats = new Stats();
-    // document.body.appendChild(stats.dom);
-
-    // Def animation and render function
+    let modelsToRotate = scene.children;
     function animate() {
         requestAnimationFrame(animate);
-
-        model_1.rotation.x += 0.01;
-        model_1.rotation.y += 0.01;
-
-        model_2.rotation.x += 0.01;
-        model_2.rotation.y += 0.01;
-
-        model_3.rotation.x += 0.01;
-        model_3.rotation.y += 0.01;
-
-        model_4.rotation.x += 0.01;
-        model_4.rotation.y += 0.01;
-
-
-        // stats.update();
+        modelsToRotate.forEach((model) => {
+            model.rotation.x += 0.01;
+            model.rotation.y += 0.01;
+        });
         renderer.render(scene, camera);
     }
-animate();
+    animate();
 }
